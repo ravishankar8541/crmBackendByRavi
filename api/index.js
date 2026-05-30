e4a0050bcdda73c231e7b2e725026bf2f55cb29e
unknown <ravishankarray480@gmail.com>
2026-05-30 16:48:09 +0530

    backend init

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 dbConnection();
 
 app.use(cors({
-  origin: 'https://crm-frontend-new-lemon.vercel.app', 
+  origin: 'http://localhost:5173', 
   credentials: true,               
 }));
 app.use(express.json());
