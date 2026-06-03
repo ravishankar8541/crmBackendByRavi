b1333d0f184106ed7684305995744564a7e073c2
unknown <ravishankarray480@gmail.com>
2026-06-03 18:25:46 +0530

    update url

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 dbConnection();
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://crm-frontend-by-ravi.vercel.app', 
   credentials: true,               
 }));
 app.use(express.json());
